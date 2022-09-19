f562ec2d347636223b2fca763fc3db05d6ce699f
Jang, Sunwoo <andyjang@iu.edu>
2022-09-18 23:05:48 -0400

    user input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <stack>
+
+void checkInput(int n, bool *invalidInput)
+{
+  if (n == 2 || n == 3)
+  {
+    std::cout << "N-Queens is not possible with " << n << ". Please enter a different number\n";
+  }
+  else
+  {
+    *invalidInput = false;
+    std::cout << "N-value: " << n << std::endl;
+  }
+}
 
 int main()
 {
-  std::cout << "Hello World!\n";
+  // Start with getting N
+  bool invalidInput = true;
+  int n = -1;
+
+  while (invalidInput)
+  {
+    std::cout << "N: ";
+    std::cin >> n;
+    std::cout << std::endl;
+    checkInput(n, &invalidInput);
+  }
+
   return 0;
 }
